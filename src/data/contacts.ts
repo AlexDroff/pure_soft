af05b8a01666e265007864939ba7c3fe500a27a3
Oleksandr Aleksandrov <alldroff@gmail.com>
2026-04-18 11:47:08 +0200

    update YouTube channel link in contacts

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -6,5 +6,5 @@ export const contacts: ContactItem = {
   whatsapp: "+34637943520",
   instagram: "https://www.instagram.com/iam_puresoft/",
   facebook: "https://www.facebook.com/profile.php?id=61586741181944",
-  youtube: "https://www.youtube.com/@puresoft",
+  youtube: "https://www.youtube.com/channel/UCDaMP5s-0Q1JEBChabiVjSg",
 };
